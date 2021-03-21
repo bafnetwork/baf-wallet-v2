82567314b76fcd917129d25d620cc9ec163ecc14
Lev Stambler <levstamb@gmail.com>
2021-03-21 12:42:02 -0400

    state

--- apps/frontend/src/App.svelte
+++ apps/frontend/src/App.svelte
@@ -1,37 +1,42 @@
 <script lang="ts">
-	import Router from 'svelte-spa-router'
-	import Login from './pages/Login.svelte'
-	import Account from './pages/Account.svelte'
-	import ApproveRedirect from './pages/ApproveRedirect.svelte'
-	import NotFound404 from './pages/NotFound404.svelte';
-	
-	const routesLoggedIn = {
-		'/': Account,
-		'/approve-redirect/:toAccount/:amount': ApproveRedirect,
-		'/*': NotFound404
-	}
-	const routesLoggedOut = {
-		'/login': Login,
-		'/:attemptedRoute': Login
-	}
+  import Router from 'svelte-spa-router';
+  import Login from './pages/Login.svelte';
+  import Account from './pages/Account.svelte';
+  import ApproveRedirect from './pages/ApproveRedirect.svelte';
+  import NotFound404 from './pages/NotFound404.svelte';
+  import { KeyStore } from './state/keys.svelte';
 
-	// TODO: implement with state 
-	async function init() : Promise<boolean> {
-		return true
-	}
-	const initProm = init()
+  const routesLoggedIn = {
+    '/': Account,
+    '/approve-redirect/:toAccount/:amount': ApproveRedirect,
+    '/*': NotFound404,
+  };
+  const routesLoggedOut = {
+    '/login': Login,
+    '/:attemptedRoute': Login,
+  };
+
+  // TODO: implement with tor.us
+  async function init(): Promise<boolean> {
+    KeyStore.set({
+      pubkey: 'Fake news',
+      privkey: 'Is real?',
+    });
+    return true;
+  }
+  const initProm = init();
 </script>
 
-<style global lang="postcss">
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-</style>
-
 {#await initProm}
-	<p>Loading...</p>
+  <p>Loading...</p>
 {:then loggedIn}
-	<Router routes={loggedIn ? routesLoggedIn : routesLoggedOut} />
+  <Router routes={loggedIn ? routesLoggedIn : routesLoggedOut} />
 {:catch error}
-	<p>An error occured loading the page</p>
+  <p>An error occured loading the page: {error.toString()}</p>
 {/await}
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
---
+++ apps/frontend/src/state/keys.svelte
@@ -0,0 +1,6 @@
+<script lang="ts" context="module">
+  import { writable } from 'svelte/store';
+  import type { KeyState } from '@baf-wallet/interfaces';
+
+  export const KeyStore = writable<KeyState | null>(null);
+</script>
