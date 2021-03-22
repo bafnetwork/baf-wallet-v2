04c636eba5834420c3f8ba87a94dec782c21c42b
Lev Stambler <levstamb@gmail.com>
2021-03-22 14:26:38 -0400

    fix rollup

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -1,5 +1,10 @@
+// Depreciated
 const sveltePreprocess = require('svelte-preprocess');
+const jsonPlug = require('@rollup/plugin-json');
 module.exports = {
+  compilerOptions: {
+    dev: true,
+  },
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess({
@@ -7,6 +12,14 @@ module.exports = {
       plugins: [
         require('tailwindcss')(__dirname + '/tailwind.config.js'),
         require('autoprefixer'),
+      ],
+    },
+    typescript: {
+      extensions: ['.svelte', '.ts'],
+      plugins: [
+        jsonPlug({
+          compact: true,
+        }),
       ],
     },
   }),
