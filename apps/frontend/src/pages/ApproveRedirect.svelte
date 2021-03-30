e1ba58eb4f4fd3e60de809e70ba8ec7aa68908f2
Sebastien La Duca <sladuca777@gmail.com>
2021-03-29 22:06:49 -0400

    approve ui

--- apps/frontend/src/pages/ApproveRedirect.svelte
+++ apps/frontend/src/pages/ApproveRedirect.svelte
@@ -3,8 +3,10 @@
   import Layout from "../components/Layout.svelte";
   import Card from "../components/base/Card.svelte";
   export let params = {} as any
-  const toAccount = params.toAccount
-  const amount = params.amount
+  // TODO: use EIP-712: https://github.com/Mrtenz/eip-712/tree/master/src if it's JSON, or expect a hash
+  const txStr = '{ "to": "0x420", "amount": 69, "data": { "msg": "BAF token when" }}';
+  const tx = JSON.parse(txStr);
+  const nonce = params.nonce
   const appName = "BAF DAO";
   // import Card from '../components/base/Card.svelte';
   // import { KeyStore } from '../state/keys.svelte';
@@ -29,12 +31,23 @@
 {:then x}
   {x}
   <Layout>
-    <Card classExtra="flex flex-col">
-      <h1 class="pb-2 text-xl">{appName} would like to execute a transaction</h1>
-      <span class="pb-2">TODO: transaction viewer</span>
+    <Card classExtra="flex flex-col divide">
+      <h1 class="pb-2 text-xl text-center">{appName} would like to execute a transaction</h1>
       <div class="flex flex-row justify-center">
-        <Button>Approve</Button>
-        <Button>Cancel</Button>
+        <span class="mx-2">To: {tx.to}</span>
+        <span class="mx-2">Amount: {tx.amount}</span>
+      </div>
+      {#if tx.data}
+        <span class="mx-2 text-center">Data</span>
+        <div class="flex flex-row justify-center">
+          <code>
+            {JSON.stringify(tx.data, null, 2)}
+          </code>
+        </div>
+      {/if}
+      <div class="flex flex-row justify-center">
+        <Button classExtra="mx-2">Approve</Button>
+        <Button classExtra="mx-2">Cancel</Button>
       </div>
     </Card>
   </Layout>
@@ -52,4 +65,5 @@
 {:catch e}
   The following error occured: {e}
 {/await}
+
 
