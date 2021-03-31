2c9a6ebf6110e569ea646a5dad3977e47713cb59
Lev Stambler <levstamb@gmail.com>
2021-03-31 16:33:52 -0400

    basic urlcreator

--- libs/redirect-generator/src/lib/interfaces/base-classes.ts
+++ libs/redirect-generator/src/lib/interfaces/base-classes.ts
@@ -1,6 +1,6 @@
-export abstract class  RedirectGenerator {
-  
-  constructor() {
+import { Chain } from '@baf-wallet/interfaces';
+export abstract class RedirectGenerator {
+  constructor(protected chain: Chain, protected baseURL: string) {}
 
-  }
+  abstract createURL(opts?: any): string | Promise<string>;
 }
---
+++ libs/redirect-generator/src/lib/near/generator.ts
@@ -0,0 +1,19 @@
+import { Chain } from '@baf-wallet/interfaces';
+import { RedirectGenerator } from '../interfaces/base-classes';
+import { transactions } from 'near-api-js';
+import { Signer } from '@baf-wallet/multi-chain';
+
+interface NearCreateUrlOpts {
+  actions: transactions.Action[];
+  receiverAccountId: string
+}
+
+export class NearGenerator extends RedirectGenerator {
+  constructor(baseURL: string) {
+    super(Chain.NEAR, baseURL);
+  }
+
+  public createURL(opts: NearCreateUrlOpts) {
+    return `${this.baseURL}/${Signer.serializeSendTXOpts(opts)}`
+  }
+}
