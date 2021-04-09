5a1471441e35b41eb2acbf4876682c28a6124a7c
Sebastien La Duca <sladuca777@gmail.com>
2021-04-08 21:39:30 -0400

    add trust-wallet-assets

---
+++ libs/trust-wallet-assets/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'trust-wallet-assets',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/trust-wallet-assets',
+};
