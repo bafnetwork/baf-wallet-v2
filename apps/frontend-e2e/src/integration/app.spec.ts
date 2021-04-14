a76268c99670fcf23de52592730db4584cae8686
Lev Stambler <levstamb@gmail.com>
2021-04-14 13:16:06 -0400

    setup cypress

--- apps/frontend-e2e/src/integration/app.spec.ts
+++ apps/frontend-e2e/src/integration/app.spec.ts
@@ -3,11 +3,11 @@ import { getGreeting } from '../support/app.po';
 describe('frontend', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it('should display account page', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to frontend!');
+    getGreeting().contains('Account');
   });
 });
