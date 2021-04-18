73e5c59bd879561c7f5d3c4c29cf8dbd7aefbeab
Lev Stambler <levstamb@gmail.com>
2021-04-18 19:44:03 -0400

    ghetto but there kinda

--- libs/baf-contract/contract/src/lib.rs
+++ libs/baf-contract/contract/src/lib.rs
@@ -1,51 +1,69 @@
-use near_sdk::env::keccak256;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::env::keccak256;
 use near_sdk::AccountId;
 use near_sdk::{collections::UnorderedMap, env, near_bindgen};
+use ed25519_dalek::Verifier;
+use std::convert::TryInto;
 
 #[global_allocator]
 static ALLOC: near_sdk::wee_alloc::WeeAlloc = near_sdk::wee_alloc::WeeAlloc::INIT;
 
-type PK = Vec<u8>;
-type EdPK = PK;
-type SecpPK = PK;
+type EdPK = [u8; 64];
+type SecpPK = [u8; 65];
 
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct AccountInfo {
-    edPK: EdPK,
-    accountId: AccountId
+    ed_pk: EdPK,
+    accountId: AccountId,
 }
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct BafContract {
     owner_id: AccountId,
-    account_infos: UnorderedMap<SecpPK, AccountInfo>
+    account_infos: UnorderedMap<SecpPK, AccountInfo>,
 }
 
 #[near_bindgen]
 impl BafContract {
     pub fn new() -> Self {
         let owner_id = env::predecessor_account_id();
-        Self { owner_id }
+        Self {
+            owner_id,
+            account_infos: UnorderedMap::new("account-infos-map".as_bytes()),
+        }
     }
 
-    pub fn set_account_info(&mut self, message: String) {
-        // keccak256(value)
-        // secp256k1::verify(
-        //     message: &Message, 
-        //     signature: &Signature, 
-        //     pubkey: &PublicKey
-        // ) -> bool
-        // https://docs.rs/ed25519-dalek/1.0.1/ed25519_dalek/struct.PublicKey.html
-    }
-    // edPK: PublicKey,
-    // edSig: string,
-    // secpPK: PublicKey,
-    // secpSig: string,
-    // newAccountID: string,
-    // msg: string
+    pub fn set_account_info(
+        &mut self,
+        userId: String,
+        nonce: String,
+        edPK: EdPK,
+        secpPK: SecpPK,
+        edSig: [u8; 64],
+        secpSig: [u8; 64],
+        newAccountID: AccountId,
+    ) -> Result<(), &str> {
+        let msg_prehash = format!("{}:{}", userId, nonce).as_bytes();
+        let hash: [u8; 32] = keccak256(msg_prehash).try_into().unwrap();
+        if !secp256k1::verify(
+            &secp256k1::Message::parse(&hash),
+            &secp256k1::Signature::parse(&secpSig),
+            // TODO: clean up
+            &secp256k1::PublicKey::parse(&secpPK).unwrap(),
+        ) {
+            return Err("");
+        }
+        let ed_pk_dalek = ed25519_dalek::PublicKey::from_bytes(&edPK).unwrap();
+        // TODO: handle fail
+        ed_pk_dalek.verify(&hash, &ed25519_dalek::Signature::new(edSig)).unwrap();
 
+        self.account_infos.insert(&secpPK, &AccountInfo {
+            ed_pk: edPK,
+            accountId: newAccountID
+        });
+        Ok(())
+    }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
