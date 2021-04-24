26c9c46c61a6b2078954d304335d46fc8d29df94
Sebastien La Duca <sladuca777@gmail.com>
2021-04-24 12:37:54 -0400

    implement some default conversions

---
+++ libs/utils/src/lib/conversions.ts
@@ -0,0 +1,58 @@
+import { Converter, SecretKey, PublicKey, Encoding, KeyPair } from '@baf-wallet/interfaces';
+import { formatBytes } from './bytes';
+import { Pair } from './types';
+import { Buffer } from 'buffer';
+
+export const bufferConverter: Converter = {
+    skFromBaf,
+    skToBaf,
+    pkFromBaf,
+    pkToBaf,
+    keyPairFromBaf,
+    keyPairToBaf
+}
+
+function skFromBaf<Curve>(bafSk: SecretKey<Curve>): Buffer {
+    return Buffer.from(bafSk.data)
+};
+
+function skToBaf<Curve>(sk: Buffer, curveMarker: Curve): SecretKey<Curve> {
+    const data = Buffer.from(sk);
+    return {
+        data,
+        curve: curveMarker,
+        format: (fmt: Encoding) => formatBytes(data, fmt)
+    }
+}
+
+function pkFromBaf<Curve>(bafPk: PublicKey<Curve>): Buffer {
+    return Buffer.from(bafPk.data);
+}
+
+
+function pkToBaf<Curve>(pk: Buffer, curveMarker: Curve): PublicKey<Curve> {
+    const data = Buffer.from(pk);
+    return {
+        data,
+        curve: curveMarker,
+        format: (fmt: Encoding) => formatBytes(data, fmt)
+    }
+};
+
+function keyPairFromBaf<Curve>(bafKeyPair: KeyPair<Curve>): Pair<Buffer, Buffer> {
+    const { pk, sk } = bafKeyPair;
+    return {
+        fst: Buffer.from(sk.data),
+        snd: Buffer.from(pk.data),
+    }
+};
+
+function keyPairToBaf<Curve>(keyPair: Pair<Buffer, Buffer>, curveMarker: Curve): KeyPair<Curve> {
+    const skBytes = keyPair.fst;
+    const pkBytes = keyPair.snd;
+    return {
+        curve: curveMarker,
+        sk: skToBaf(skBytes, curveMarker),
+        pk: pkToBaf(pkBytes, curveMarker)
+    }
+}
