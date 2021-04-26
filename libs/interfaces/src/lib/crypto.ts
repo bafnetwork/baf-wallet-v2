662e44a047a3eb0bd5157ad38cc43af5b23392c0
Sebastien La Duca <sladuca777@gmail.com>
2021-04-25 23:37:34 -0400

    add crypto types from draft pr

--- libs/interfaces/src/lib/crypto.ts
+++ libs/interfaces/src/lib/crypto.ts
@@ -1,13 +1,40 @@
-export enum CryptoCurves {
-  secp256k1 = 'secp256k1',
-  ed25519 = 'ed25519',
+export type secp256k1 = {
+  toString: () => "secp256k1"
+};
+
+export type ed25519 = {
+  toString: () => "ed25519"
+};
+
+export const secp256k1Marker: secp256k1 = {
+  toString: () => "secp256k1"
+};
+
+export const ed25519Marker: ed25519 = {
+  toString: () => "ed25519"
+};
+
+export type SupportedCurve = secp256k1 | ed25519;
+
+export enum Encoding {
+  bs58,
+  hex,
 }
 
-export enum KeyFormats {
-  BS58,
-  HEX,
-  ARRAY,
+export interface PublicKey<Curve> {
+  data: Buffer;
+  curve: Curve;
+  format: (fmt: Encoding) => string;
 }
 
-export type PublicKey = Buffer;
-export type SecretKey = Buffer;
+export interface SecretKey<Curve> {
+  data: Buffer;
+  curve: Curve;
+  format: (fmt: Encoding) => string;
+}
+
+export interface KeyPair<Curve> {
+  curve: Curve;
+  sk: SecretKey<Curve>;
+  pk: PublicKey<Curve>;
+}
