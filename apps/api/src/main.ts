6022de436187fd146daa966533b2f3e73483acf6
Lev Stambler <levstamb@gmail.com>
2021-04-30 18:04:55 -0400

    port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -25,25 +25,25 @@ async function init() {
     })
   );
   app.use(bodyParser.json());
-  // const whitelist = [
-  //   'http://localhost:8080',
-  //   'https://baf-wallet.netlify.app',
-  //   'https://baf-wallet-v2-git-deploy-work-baf-wallet.vercel.app',
-  // ];
-  // const corsOptions = {
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  // };
-  // app.use(cors(corsOptions));
+  const whitelist = [
+    'http://localhost:8080',
+    'https://baf-wallet.netlify.app',
+    'https://baf-wallet-v2-git-deploy-work-baf-wallet.vercel.app',
+  ];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
+  app.use(cors(corsOptions));
 
   RegisterRoutes(app);
 
-  const port = process.env.port || 3333;
+  const port = process.env.PORT || 3333;
   const server = app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/`);
   });
