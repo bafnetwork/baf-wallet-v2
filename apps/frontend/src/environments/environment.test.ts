160c7957bd6561cda1a215038956ceda0c5b86b7
Lev Stambler <levstamb@gmail.com>
2021-04-30 14:06:00 -0400

    basePath

--- apps/frontend/src/environments/environment.test.ts
+++ apps/frontend/src/environments/environment.test.ts
@@ -3,7 +3,6 @@ import { Env } from '@baf-wallet/interfaces';
 export const environment = {
   production: false,
   env: Env.TEST,
-  baseUrl: 'https://baf-wallet.netlify.app',
-  // TODO: update
+  baseUrl: 'https://baf-wallet-v2.vercel.app',
   basePathApi: 'https://baf-wallet-testnet.herokuapp.com',
 };
