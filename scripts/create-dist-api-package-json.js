805087a161171bc74f3a09850ab365ca606bb89c
Sebastien La Duca <sladuca777@gmail.com>
2021-05-04 13:04:25 -0400

    had a bruh moment

--- scripts/create-dist-api-package-json.js
+++ scripts/create-dist-api-package-json.js
@@ -2,7 +2,7 @@ const package = require('../package.json');
 const fs = require('fs');
 
 fs.writeFileSync(
-  'dist/apps/bot/package.json',
+  'dist/apps/api/package.json',
   JSON.stringify({
     main: 'main.js',
     scripts: { start: 'node main.js' },
--- scripts/create-dist-bot-package-json.js
+++ scripts/create-dist-bot-package-json.js
@@ -2,7 +2,7 @@ const package = require('../package.json');
 const fs = require('fs');
 
 fs.writeFileSync(
-  'dist/apps/api/package.json',
+  'dist/apps/bot/package.json',
   JSON.stringify({
     main: 'main.js',
     scripts: { start: 'node main.js' },
